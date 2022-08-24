ad990d74a41c7acd7b1f26c625210c4a86332e86
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2022-08-24 17:05:25 +0800

    SpringCloud - Microservice setup

---
+++ guest-services/src/main/java/com/alexo/lil/guesetservices/GuestRepository.java
@@ -0,0 +1,6 @@
+package com.alexo.lil.guesetservices;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface GuestRepository extends CrudRepository<Guest, Long> {
+}
---
+++ guest-services/src/test/java/com/alexo/lil/guesetservices/GuesetServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexo.lil.guesetservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuesetServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
